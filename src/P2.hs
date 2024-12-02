049e8dd65c51ea0e4ae4f434e450c41c6e7a6054
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2024-12-02 15:49:49 +1030

    Day 2 tidy

--- src/P2.hs
+++ src/P2.hs
@@ -10,24 +10,36 @@ run2 = solve2 . parse
 inputLocation :: String
 inputLocation = "inputs/input2"
 
+parse :: String -> [[Int]]
 parse = map parseLine . lines
 
 parseLine :: String -> [Int]
 parseLine = map read . words
 
+solve1 :: [[Int]] -> Int
 solve1 = length . filter isSafe
 
-isSafe input = isMonotonic input && differsByAtMost3 input
+isSafe :: [Int] -> Bool
+isSafe input = isMonotonic diffs && differsByAtMost3 diffs
+    where diffs = zipWith (-) input $ tail input
 
-isMonotonic input = allEqual $ map signum $ zipWith (-) input $ tail input
+isMonotonic :: [Int] -> Bool
+isMonotonic = allEqual . map signum
 
+allEqual :: [Int] -> Bool
 allEqual = (<= 1) . length . nub
 
 differsByAtMost3 :: [Int] -> Bool
-differsByAtMost3 input = all ((<= 3) . abs) $ zipWith (-) input $ tail input
+differsByAtMost3 = all ((<= 3) . abs)
 
+solve2 :: [[Int]] -> Int
 solve2 = length . filter isAlmostSafe
 
+isAlmostSafe :: [Int] -> Bool
 isAlmostSafe = any isSafe . safeVariants
 
-safeVariants input = [input] ++ map (\n -> take n input ++ drop (n+1) input) [0..length input]
+safeVariants :: [a] -> [[a]]
+safeVariants input = input : map (removeNth input) [0..length input]
+
+removeNth ::[a] ->  Int -> [a]
+removeNth xs n = take n xs ++ drop (n + 1) xs
