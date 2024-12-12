2fc72c07c8d0c6ac893102fb7904209c1848037f
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2024-12-12 19:22:27 +1030

    Day 12 tidy

--- src/P12.hs
+++ src/P12.hs
@@ -7,17 +7,22 @@ import Data.List (groupBy, sortOn, sort)
 
 data Edge = Edge {direction :: Direction, level :: Int, loc :: Int} deriving Show
 
+run1 :: String -> Int
 run1 = solve1 . parse
 
+run2 :: String -> Int
 run2 = solve2 . parse
 
 inputLocation :: String
 inputLocation = "inputs/input12"
 
+parse :: String -> M.Map Coord Char
 parse = textToCoordMap
 
+solve1 :: M.Map Coord Char -> Int
 solve1 = sum . map cost . regions
 
+regions :: M.Map Coord Char -> [[Coord]]
 regions m = case M.toList m of
                 [] -> []
                 ((c,x):_) ->
@@ -33,34 +38,44 @@ makeRegion x (region, m) c
         in  foldl (makeRegion x) (region', m') neighbours'
     | otherwise = (region, m)
 
+neighbours :: Coord -> [Coord]
 neighbours (x,y) = [(x+1,y),(x-1,y),(x,y-1),(x,y+1)]
 
-cost region = area * (perimeter region)
+cost :: [Coord] -> Int
+cost region = area * perimeter region
     where area = length region
 
+perimeter :: [Coord] -> Int
 perimeter region = sum $ map (sideLength (S.fromList region)) region
 
+sideLength :: S.Set Coord -> Coord -> Int
 sideLength region c = length $ filter (`S.notMember` region) (neighbours c)
 
+solve2 :: M.Map Coord Char -> Int
 solve2 =  sum . map cost2 . regions
 
-cost2 region = area * (sides region)
+cost2 :: [Coord] -> Int
+cost2 region = area * sides region
     where area = length region
 
+sides :: [Coord] -> Int
 sides = countSides . edges
 
 edges :: [Coord] -> [Edge]
 edges region = concatMap (edges' (S.fromList region)) region
 
+edges' :: S.Set Coord -> (Int, Int) -> [Edge]
 edges' region c =
     let xs = filter (`S.notMember` region) (neighbours c)
     in  map (edge c) xs
 
+edge :: Coord -> Coord -> Edge
 edge from@(x,y) to
     | d == (1,0) = Edge DirLeft x y
     | d == (-1,0) = Edge DirRight x y
     | d == (0,1) = Edge DirDown y x
     | d == (0,-1) = Edge DirUp y x
+    | otherwise = error "bad neighbour"
     where d = subtractCoords to from
 
 countSides :: [Edge] -> Int
@@ -72,6 +87,7 @@ countSidesDir xs = sum $ map (countSidesLoc . map loc) $ groupBy (\a b -> level 
 countSidesLoc :: [Int] -> Int
 countSidesLoc = countContiguous . sort
 
+countContiguous :: [Int] -> Int
 countContiguous [] = 0
 countContiguous (x:xs) = 1 + go x xs
     where go _ [] = 0
