2a118c6035f1a12bd1cc833a0fb18ac306382e36
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2024-12-13 16:46:55 +1030

    Day 13 tidy

--- src/P13.hs
+++ src/P13.hs
@@ -1,49 +1,45 @@
 module P13 (run1, run2, inputLocation) where
 import Data.List.Split (splitOn)
-import Lib (subtractCoords)
-import Data.Maybe (mapMaybe)
+import Lib (Coord)
 
+data Machine = Machine Coord Coord Coord -- button a, button b, target
+
+run1 :: String -> Int
 run1 = solve1 . parse
 
+run2 :: String -> Int
 run2 = solve2 . parse
 
 inputLocation :: String
 inputLocation = "inputs/input13"
 
-parse = map parseItem . splitOn [""] . lines
+parse :: String -> [Machine]
+parse = map parseMachine . splitOn [""] . lines
 
-parseItem (a:b:prize:_) = (parseButton (words a), parseButton (words b), parsePrize (words prize))
+parseMachine :: [String] -> Machine
+parseMachine (a:b:prize:_) = Machine (parseButton (words a)) (parseButton (words b)) (parsePrize (words prize))
+parseMachine _ = error "Bad input"
 
+parseButton :: [String] -> Coord
 parseButton s = (parseCoord (init (s !! 2)), parseCoord (s !! 3))
 
 parseCoord :: String -> Int
 parseCoord = read . drop 2
 
+parsePrize :: [String] -> Coord
 parsePrize s = (parseCoord (init (s !! 1)), parseCoord (s !! 2))
 
-solve1 = sum . map fewestTokensToWin
+solve1 :: [Machine] -> Int
+solve1 = sum . map tokenCost
 
-fewestTokensToWin machine = safeMin $ mapMaybe (cost machine) [0..100]
+solve2 :: [Machine] -> Int
+solve2 = solve1 . map shiftPrize
 
-cost (a@(xa, ya), b@(xb, yb), prize@(xp, yp)) aPresses =
-    let target = subtractCoords prize (xa * aPresses, ya * aPresses)
-    in  case dropWhile (isBefore target . mulCoord b) [0..100] of
-            [] -> Nothing
-            (c:_) | (mulCoord b c) == target -> Just (c + 3*aPresses)
-            _ -> Nothing
+shiftPrize :: Machine -> Machine
+shiftPrize (Machine a b (x,y)) = Machine a b (x+10000000000000,y+10000000000000)
 
-safeMin [] = 0
-safeMin xs = minimum xs
-
-mulCoord (x,y) n = (n*x,n*y)
-
-isBefore (x1,y1) (x2,y2) = x2 < x1 && y2 <y1
-
-solve2 = sum . map fewestTokensToWin2 . map shiftPrize
-
-shiftPrize (a, b, (x,y)) = (a, b, (x+10000000000000,y+10000000000000))
-
-fewestTokensToWin2 ((xa, ya), (xb, yb), (xt, yt)) =
+tokenCost :: Machine -> Int
+tokenCost (Machine (xa, ya) (xb, yb) (xt, yt)) =
     let denB = xt * ya - yt * xa
         numB = xb * ya - xa * yb
         denA = xt * yb - yt * xb
