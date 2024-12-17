e2568a53bc7a8016592a07874a5921600500bae9
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2024-12-17 11:19:41 +1030

    Added timing info

--- app/Main.hs
+++ app/Main.hs
@@ -1,5 +1,6 @@
 module Main (main) where
 
+import Data.Time
 import P16
 
 main :: IO ()
@@ -7,5 +8,13 @@ main = do
     input <- readFile inputLocation
     let result1 = run1 input
         result2 = run2 input
+    start1 <- getCurrentTime
     print result1
+    end1 <- getCurrentTime
+    let diff1 = diffUTCTime end1 start1
+    putStrLn $ "Execution time: " ++ show diff1
+    start2 <- getCurrentTime
     print result2
+    end2 <- getCurrentTime
+    let diff2 = diffUTCTime end2 start2
+    putStrLn $ "Execution time: " ++ show diff2
