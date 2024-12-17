73e74b51aac7b28023c197f1e36438876489cef0
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2024-12-17 12:03:10 +1030

    Dijkstra performance improvements

--- src/Dijkstra16.hs
+++ src/Dijkstra16.hs
@@ -5,15 +5,16 @@ import qualified Data.Set as S
 import Data.List (delete)
 
 type Node a = M.Map a Int
-type UnvisitedNodes a = M.Map a (Maybe (Int, S.Set a))
+type VisitedNodes a = S.Set a
+type UnvisitedNodes a = M.Map a (Int, S.Set a)
 type Nodes a = M.Map a (Node a)
 
 dijkstra :: Ord a => Nodes a -> a -> [a] -> Maybe (Int, S.Set a)
-dijkstra nodes start = dijkstra' nodes (M.map (const Nothing) nodes) (Just (start, (0, S.singleton start))) Nothing
+dijkstra nodes start = dijkstra' nodes S.empty M.empty (Just (start, (0, S.singleton start))) Nothing
 
-dijkstra' :: Ord a => Nodes a -> UnvisitedNodes a -> Maybe (a, (Int, S.Set a)) -> Maybe (Int, S.Set a) -> [a] -> Maybe (Int, S.Set a)
-dijkstra' _ _ Nothing _ _ = Nothing
-dijkstra' nodes unvisitedNodes (Just (currentNode, nodeState@(newDistance, newPaths))) prevResult targetNodes
+dijkstra' :: Ord a => Nodes a -> VisitedNodes a -> UnvisitedNodes a -> Maybe (a, (Int, S.Set a)) -> Maybe (Int, S.Set a) -> [a] -> Maybe (Int, S.Set a)
+dijkstra' _ _ _ Nothing _ _ = Nothing
+dijkstra' nodes visited unvisitedNodes (Just (currentNode, nodeState@(newDistance, newPaths))) prevResult targetNodes
     | currentNode `elem` targetNodes =
         let targetNodes' = currentNode `delete` targetNodes
             result = case prevResult of
@@ -26,24 +27,28 @@ dijkstra' nodes unvisitedNodes (Just (currentNode, nodeState@(newDistance, newPa
                             else error "Should never encounter a shorter path than previously found"
         in  if null targetNodes' || maybe False ((< newDistance) . fst) prevResult
             then result
-            else dijkstraStep nodes result targetNodes' unvisitedNodes'
-    | otherwise = dijkstraStep nodes prevResult targetNodes unvisitedNodes'
-        where unvisitedNodes' = updateUnvisited nodes nodeState currentNode unvisitedNodes
+            else dijkstraStep nodes result targetNodes' visited unvisitedNodes'
+    | otherwise = dijkstraStep nodes prevResult targetNodes visited unvisitedNodes'
+        where unvisitedNodes' = updateUnvisited nodes nodeState currentNode visited unvisitedNodes
 
-dijkstraStep :: Ord a => Nodes a -> Maybe (Int, S.Set a) -> [a] -> UnvisitedNodes a -> Maybe (Int, S.Set a)
-dijkstraStep nodes prevResult targetNodes unvisitedNodes =
+dijkstraStep :: Ord a => Nodes a -> Maybe (Int, S.Set a) -> [a] -> VisitedNodes a -> UnvisitedNodes a -> Maybe (Int, S.Set a)
+dijkstraStep nodes prevResult targetNodes visited unvisitedNodes =
     let nextNode = smallestDistance unvisitedNodes
-    in  dijkstra' nodes unvisitedNodes nextNode prevResult targetNodes
+    in  dijkstra' nodes (updateVisited visited nextNode) unvisitedNodes nextNode prevResult targetNodes
 
-updateUnvisited :: Ord a => Nodes a -> (Int, S.Set a) -> a -> UnvisitedNodes a -> UnvisitedNodes a
-updateUnvisited nodes currentDistance currentNode unvisitedNodes = M.delete currentNode $ updateNeighbours nodes currentDistance unvisitedNodes currentNode
+updateVisited :: Ord a => S.Set a -> Maybe (a, b) -> S.Set a
+updateVisited visited Nothing = visited
+updateVisited visited (Just (n, _)) = S.insert n visited
 
-updateNeighbours :: Ord a => Nodes a -> (Int, S.Set a) -> UnvisitedNodes a -> a -> UnvisitedNodes a
-updateNeighbours nodes currentDistance unvisitedNodes = foldl (updateNeighbour currentDistance) unvisitedNodes . M.toList . (nodes M.!)
+updateUnvisited :: Ord a => Nodes a -> (Int, S.Set a) -> a ->  VisitedNodes a -> UnvisitedNodes a -> UnvisitedNodes a
+updateUnvisited nodes currentDistance currentNode visited unvisitedNodes = M.delete currentNode $ updateNeighbours nodes currentDistance visited unvisitedNodes currentNode
+
+updateNeighbours :: Ord a => Nodes a -> (Int, S.Set a) -> VisitedNodes a -> UnvisitedNodes a -> a -> UnvisitedNodes a
+updateNeighbours nodes currentDistance visitedNodes unvisitedNodes = foldl (updateNeighbour currentDistance) unvisitedNodes . filter ((`S.notMember` visitedNodes) . fst) . M.toList . (nodes M.!)
 
 updateNeighbour :: Ord a => (Int, S.Set a) -> UnvisitedNodes a -> (a, Int) -> UnvisitedNodes a
 updateNeighbour (currentDistance, currentPath) unvisitedNodes (key, distance) =
-    M.adjust (updateDistance (currentDistance + distance) (S.insert key currentPath)) key unvisitedNodes
+    M.alter (updateDistance (currentDistance + distance) (S.insert key currentPath)) key unvisitedNodes
 
 smallestDistance :: UnvisitedNodes a -> Maybe (a, (Int, S.Set a))
 smallestDistance = M.foldlWithKey minTentativeDistance Nothing
@@ -55,9 +60,8 @@ updateDistance dNew pathNew old@(Just (dOld, pathOld))
     | dNew < dOld = Just (dNew, pathNew)
     | otherwise = old
 
-minTentativeDistance :: Maybe (a, (Int, S.Set a)) -> a -> Maybe (Int, S.Set a) -> Maybe (a, (Int, S.Set a))
-minTentativeDistance prev _ Nothing = prev
-minTentativeDistance Nothing key (Just prev) = Just (key, prev)
-minTentativeDistance prev@(Just (_, (prevDistance, _))) key (Just next@(tentativeDistance, _))
+minTentativeDistance :: Maybe (a, (Int, S.Set a)) -> a -> (Int, S.Set a) -> Maybe (a, (Int, S.Set a))
+minTentativeDistance Nothing key prev = Just (key, prev)
+minTentativeDistance prev@(Just (_, (prevDistance, _))) key next@(tentativeDistance, _)
     | tentativeDistance < prevDistance = Just (key, next)
     | otherwise = prev
