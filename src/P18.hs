e4975c80c4db84496fb1c650843688107c068938
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2024-12-18 15:57:06 +1030

    Day 18 tidy

--- src/P18.hs
+++ src/P18.hs
@@ -8,29 +8,40 @@ import Dijkstra (dijkstra)
 import Data.Maybe (isNothing)
 import Data.List (inits, find)
 
+run1 :: String -> Maybe Int
 run1 = solve1 . parse
 
+run2 :: String -> Maybe Coord
 run2 = solve2 . parse
 
 inputLocation :: String
 inputLocation = "inputs/input18"
 
+parse :: String -> [Coord]
 parse = map parseLine . lines
 
+parseLine :: String -> Coord
 parseLine = parseCoord . splitOn ","
 
 parseCoord :: [String] -> Coord
 parseCoord (x:y:_) = (read x, read y)
+parseCoord e = error ("Bad coord input: " ++ show e)
 
-solve1 input = dijkstra (nodes (S.fromList $ take 1024 input)) (0,0) [(70,70)]
+solve1 :: [Coord] -> Maybe Int
+solve1 = solve . take 1024
 
-nodes walls = M.fromList $ map (\c -> (c, graph c (S.fromList freeNodes))) freeNodes
+nodes :: S.Set Coord -> M.Map Coord (M.Map Coord Int)
+nodes walls = M.fromList $ map (\c -> (c, graph (S.fromList freeNodes) c)) freeNodes
     where freeNodes = filter (`S.notMember` walls) [(x,y) | x <- [0..70], y <- [0..70]]
 
-graph :: Coord -> S.Set Coord -> M.Map Coord Int
-graph (x,y) freeNodes = M.fromList $ map (\c -> (c,1)) $ filter (`S.member` freeNodes) neighbours
-    where neighbours = [(x+1,y),(x-1,y),(x,y-1),(x,y+1)]
+graph :: S.Set Coord -> Coord -> M.Map Coord Int
+graph freeNodes = M.fromList . map (\c -> (c,1)) . filter (`S.member` freeNodes) . neighbours
 
+neighbours :: Coord -> [Coord]
+neighbours (x,y) = [(x+1,y),(x-1,y),(x,y-1),(x,y+1)]
+
+solve :: [Coord] -> Maybe Int
 solve input = dijkstra (nodes (S.fromList input)) (0,0) [(70,70)]
 
-solve2 input = find (isNothing . solve) (drop 1024 $ inits input)
+solve2 :: [Coord] -> Maybe Coord
+solve2 input = last <$> find (isNothing . solve) (drop 1024 $ inits input)
