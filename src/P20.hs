199986693a5a3551389eed77aac24664e90bb24c
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2024-12-20 17:16:50 +1030

    Day 20 tidy

--- src/P20.hs
+++ src/P20.hs
@@ -2,37 +2,53 @@ module P20 (run1, run2, inputLocation) where
 
 import qualified Data.Map as M
 import qualified Data.Set as S
-import Lib (textToCoordMap)
-import qualified Dijkstra20 as D20
+import Lib (textToCoordMap, Coord)
+import Data.List (find)
 
+type Map = M.Map Coord Char
+
+run1 :: String -> Int
 run1 = solve1 . parse
 
+run2 :: String -> Int
 run2 = solve2 . parse
 
 inputLocation :: String
 inputLocation = "inputs/input20"
 
-parse = textToCoordMap
+parse :: String -> [Coord]
+parse s = path (M.keysSet $ M.filter (/= '#') m) (startNode m)
+    where m = textToCoordMap s
 
-solve1 x = findCheats 2 100 <$> bestPath x
+solve1 :: [Coord] -> Int
+solve1 = findCheats 2 100
 
-bestPath m = snd <$> D20.dijkstra (adjGraph m) (start m) [(end m)]
-
-adjGraph m =
-    let freeNodes = M.keysSet $ M.filter (/= '#') m
-    in  M.fromList $ map (\n -> (n, adjNodes freeNodes n)) $ S.toList freeNodes
-
-adjNodes m n = M.fromList $ map (\n' -> (n', 1)) $ filter (`S.member` m) $ adjCoords n
-
+adjCoords :: Coord -> [Coord]
 adjCoords (x, y) = [(x, y-1), (x, y+1), (x-1, y), (x+1, y)]
 
-start m = head $ M.keys $ M.filter (== 'S') m
+startNode :: Map -> Coord
+startNode = head . M.keys . M.filter (== 'S')
 
-end m = head $ M.keys $ M.filter (== 'E') m
-
+findCheats :: Int -> Int -> [Coord] -> Int
 findCheats _ _ [] = 0
-findCheats cheatTime saved (x:xs) = length (filter (\(honest, cheat) -> cheat <= cheatTime && honest >= cheat + saved) $ zip [1..] $ map (distance x) xs) + findCheats cheatTime saved xs
+findCheats cheatTime toSave (x:xs) = findCheatsFrom cheatTime toSave x xs + findCheats cheatTime toSave xs
 
-solve2 x = findCheats 20 100 <$> bestPath x
+findCheatsFrom :: Int -> Int -> Coord -> [Coord] -> Int
+findCheatsFrom cheatTime toSave start = length . filter (>= toSave) . zipWith (cheatTimeSaved cheatTime start) [1..]
 
+cheatTimeSaved :: Int -> Coord -> Int -> Coord -> Int
+cheatTimeSaved maxCheat start honestDistance end
+    | cheatTime <= maxCheat = honestDistance - cheatTime
+    | otherwise = 0
+    where cheatTime = distance start end
+
+solve2 :: [Coord] -> Int
+solve2 = findCheats 20 100
+
+distance :: Coord -> Coord -> Int
 distance (x1, y1) (x2, y2) = abs (x1 - x2) + abs (y1 - y2)
+
+path :: S.Set Coord -> Coord -> [Coord]
+path m loc = case find (`S.member` m) (adjCoords loc) of
+    Nothing -> [loc]
+    Just next -> loc : path (S.delete loc m) next
