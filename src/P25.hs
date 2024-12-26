37a5768b0ae01f84c0a8840106f4107a4c6f473d
Jonathan Travaglia <jonathan.travaglia@gmail.com>
2024-12-26 10:48:46 +1030

    Day 25 tidy

--- src/P25.hs
+++ src/P25.hs
@@ -1,41 +1,34 @@
 module P25 (run1, run2, inputLocation) where
 
-import qualified Data.Map as M
-import qualified Data.Set as S
 import Data.List.Split (splitOn)
 import Data.List (transpose)
 
 data Item = Key [Int] | Lock [Int]
 
+run1 :: String -> Int
 run1 = solve1 . parse
 
+run2 :: String -> Integer
 run2 = solve2 . parse
 
 inputLocation :: String
 inputLocation = "inputs/input25"
 
+parse :: String -> [Item]
 parse = map parseItem . splitOn [""] . lines
 
+parseItem :: [String] -> Item
 parseItem xs =
     if head (head xs) == '#'
     then Lock (map (length . takeWhile (== '#')) (transpose xs))
     else Key (map (length . takeWhile ( == '#') . reverse) (transpose xs))
 
-parseLine = map (const "") . words
+solve1 :: [Item] -> Int
+solve1 items = length $ filter id $ [canUnlock a b | a <- items, b <- items]
 
-solve1 items = 
-    let keys = filter isKey items
-        locks = filter isLock items
-    in  sum $ map (unlocks locks) keys
-
-isKey (Key _) = True
-isKey _ = False
-
-isLock (Lock _) = True
-isLock _ = False
-
-unlocks locks key = length $ filter (canUnlock key) locks
-
+canUnlock :: Item -> Item -> Bool
 canUnlock (Key key) (Lock lock) = all (<=7) $ zipWith (+) key lock
+canUnlock _ _ = False
 
+solve2 :: b -> Integer
 solve2 = const 0
